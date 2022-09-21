e8f6e5e3ed4b08759792a4adcc2d1437d9ceda79
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-09-21 10:27:51 +0530

    fetched the data into the card component and did error 404 handling as well

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const APIURL = 'https://api.github.com/users/'
 
 const form = document.getElementById('form')
+const main = document.getElementById('main')
 const search = document.getElementById('search')
 
 // getUser('GAURAVKR007')
@@ -15,11 +16,48 @@ async function getUser(username) {
     try{
         const {data} = await axios(APIURL + username)
 
-    console.log(data);
+    createUserCard(data)
     }catch(err){
         console.log(err);
+        if(err.response.status == 404) {
+            createErrorCard('No User Found [Error 404]')
+        }
     }
     
+}
+
+function createErrorCard(msg){
+    const cardHTML = `
+    <div class = "card">
+            <h1>${msg}</h1>
+            </div>
+    `
+
+    main.innerHTML = cardHTML
+}
+
+function createUserCard(user){
+    const cardHTML = `
+    <div class="card">
+        <div>
+          <img src="${user.avatar_url}" alt="${user.name}" class="avatar"> 
+        </div>
+        <div class="user-info">
+          <h2>${user.name}</h2>
+          <p>${user.bio}</p>
+
+          <ul>
+            <li>${user.followers}<strong>Followers</strong></li>
+            <li>${user.following}<strong>Following</strong></li>
+            <li>${user.public_repos}<strong>Repos</strong></li>
+          </ul>
+
+          <div id="repos"></div>
+        </div>
+      </div>
+    `
+    main.innerHTML = cardHTML
+
 }
 
 form.addEventListener('submit',(e)=>{
